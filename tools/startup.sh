a8a248a370bf2365bf78218ff48eb478b6d986b7
Peter Lord <im@ptrlrd.com>
2026-04-06 17:56:53 -0700

    Update startup.sh to support --beta and --beta-only flags

--- tools/startup.sh
+++ tools/startup.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+set -e
+
 git pull
-docker compose -f docker-compose.prod.yml pull
-docker compose down
-docker compose -f docker-compose.prod.yml pull && docker compose -f docker-compose.prod.yml up -d
+
+if [ "$1" = "--beta" ]; then
+    # Start both prod and beta
+    docker compose -f docker-compose.prod.yml pull
+    docker compose -f docker-compose.prod.yml down
+    docker compose -f docker-compose.prod.yml up -d
+
+    docker compose -f docker-compose.beta.yml pull
+    docker compose -f docker-compose.beta.yml down
+    docker compose -f docker-compose.beta.yml up -d
+elif [ "$1" = "--beta-only" ]; then
+    # Start only beta
+    docker compose -f docker-compose.beta.yml pull
+    docker compose -f docker-compose.beta.yml down
+    docker compose -f docker-compose.beta.yml up -d
+else
+    # Start only prod (default)
+    docker compose -f docker-compose.prod.yml pull
+    docker compose -f docker-compose.prod.yml down
+    docker compose -f docker-compose.prod.yml up -d
+fi
